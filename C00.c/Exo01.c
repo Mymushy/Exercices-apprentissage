62e471df955408f2e2b00742dc3e7bda6dde1d15
pierre.fouesnant23@gmail.com <mushy@DESKTOP-4A8CIB6>
2023-12-13 19:37:18 +0100

    exo01

---
+++ C00.c/Exo01.c
@@ -0,0 +1,17 @@
+    
+    #include <unistd.h>
+
+void ft_print_alphabet(void) {
+    char current_char = 'a';
+
+    while (current_char <= 'z') {
+        write(1, &current_char, 1);
+        current_char++;
+    }
+
+}
+
+int main() {
+    ft_print_alphabet();
+    return 0;
+}
