5ecd128525f73af2ee47b7361f8168f7ed2536cf
pierre.fouesnant23@gmail.com <pierre.fouesnant23@gmail.com>
2023-12-13 20:50:06 +0100

    enfin

---
+++ C00.c/Exo02.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void) {
+    char current_char = 'z';
+
+    while (current_char >= 'a') {
+        write(1, &current_char, 1);
+        current_char--;
+    }
+
+}
+
+int main() {
+    ft_print_reverse_alphabet();
+    return 0;
+}
+
