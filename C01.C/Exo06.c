082f34570a6df0927b26ba897812f4562ebda866
pierre.fouesnant23@gmail.com <pierre.fouesnant23@gmail.com>
2024-02-16 19:04:14 +0100

    exo06

---
+++ C01.C/Exo06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int ft_strlen(char *str) 
+{
+int longueur = 0;
+    while (*str != '\0') {
+        longueur++;
+        str++;
+    }
+    return longueur;
+}
+
+int main()
+{
+    char *str = "Pas facile de tout comprendnre mais ca va";
+    printf("Le mot contient = %d\n", ft_strlen(str));
+    return(0);
+}
