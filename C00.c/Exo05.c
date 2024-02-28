07864937559fc10f00d25544e638d216b3840e38
pierre.fouesnant23@gmail.com <pierre.fouesnant23@gmail.com>
2024-02-28 20:48:53 +0100

    oh

--- C00.c/Exo05.c
+++ C00.c/Exo05.c
@@ -1,37 +1,43 @@
 #include <unistd.h>
 
-void ft_print_comb(void){
+void ft_print_comb(void)
+{
     char centaine = '0';
     char dizaine = '0';
     char unite = '0';
 
-    while ( centaine <= '7' ) {
-    while ( dizaine <= '8' ) {
-    while ( unite <= '9' ) {
+    while ( centaine <= '7' ) 
+    {
+        while ( dizaine <= '8' ) 
+        {
+            while ( unite <= '9' ) 
+            {
 
     
-        write(1, &centaine, 1);
-        write(1, &dizaine, 1);    
-        write(1, &unite, 1); 
+                write(1, &centaine, 1);
+                write(1, &dizaine, 1);    
+                write(1, &unite, 1); 
 
-        if (!(centaine == '7' && dizaine == '8' && unite == '9')) {
-    write(1, ", ", 2);
-}
+                 if (!(centaine == '7' && dizaine == '8' && unite == '9')) 
+                {
+                     write(1, ", ", 2);
+                }
 
         unite++;
-        }
+            }
 
         dizaine++;
         unite = dizaine + 1;
-    }
+        }
 
     centaine++;
     dizaine = centaine + 1;
     unite = dizaine + 1;
-}
+    }
 }   
 
-int main() {
+int main() 
+{
     ft_print_comb();
     write(1, "\n", 1);
     return 0;
