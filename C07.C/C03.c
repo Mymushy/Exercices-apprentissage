0a57bcb548697ae357acbbcfa119bb13084fea07
pierre.fouesnant23@gmail.com <pierre.fouesnant23@gmail.com>
2024-07-19 00:06:30 +0200

    exo07

--- C07.C/C03.c
+++ C07.C/C03.c
@@ -9,16 +9,51 @@ char *ft_strjoin(int size, char **strs, char *sep)
     int i = 0;
     int j = 0;
     int k = 0;
-    int *chaine;
+    int st1 = 0;
+    int st2 = 0;
+    int sizetot;
 
+    if (size == 0)
+    {   
+        char *cVidefrère = (char*)malloc(1);
+        
+        if(cVidefrère == NULL)
+            return NULL;
+        
+        cVidefrère[0]= '\0';
+        return cVidefrère;
+    }
+    
+    while (sep[st2] != '\0')
+        st2++;
+    
+    while ( i < size)
+    {
+        int j = 0;
+        while (strs[i][j] != '\0')
+        {
+            j++;
+            st1++;
+        }
 
-    chaine = (char*)malloc(sizeof(strs));
+        i++;
+        
+    }
+    
+    sizetot = st1 + st2;
+
+    char *chaine = (char*)malloc((sizetot + 1 ) * sizeof(char));
     
     if (chaine == NULL)
         return NULL;
 
+    k = 0;
+    i = 0;
+
     while ( i < size)
     {
+        j = 0;
+
         while (strs[i][j] != '\0')
         {
             chaine[k] = strs[i][j];
@@ -26,6 +61,19 @@ char *ft_strjoin(int size, char **strs, char *sep)
             j++;
         }
 
+        if (i < size - 1)
+        {
+            j = 0;
+
+            while (sep[j] != '\0') 
+            {
+                chaine[k] = sep[j];
+                k++;
+                j++;
+            }
+        }
+    
+        i++;
     }
     
 
@@ -41,25 +89,14 @@ int main()
 {
     char *strs [] = {"Lamine", "YAMAL"};
     char *sep = " ";
-    int size = sizeof(*strs);
-
-    printf("Le résultat = %s\n", ft_strjoin(size, strs, sep));
-    return(0);
+    int size = sizeof(strs) / sizeof(strs[0]);
+   
     
-}    
-    
-    
-    /*
-    > BONUS POUR SERVIR DE RAPPEL PLUS TARD --> UTILISATION DE FREE
-
-    char *src = "ralphlauren";
-    char *result = ft_strdup(src);
-    
+    char *result = ft_strjoin(size, strs, sep);
     if (result != NULL)
     {
         printf("Le résultat = %s\n", result);
-        free(result); // Libérer la mémoire allouée par ft_strdup
+        free(result); 
     }
-
-    return 0;
-    */
+    return(0);
+}    
