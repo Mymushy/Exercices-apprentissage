54451e0d9a59b30ce7859ec284d2bd73070eb75f
pierre.fouesnant23@gmail.com <pierre.fouesnant23@gmail.com>
2024-08-12 22:07:37 +0200

    final

---
+++ C07.C/C05.c
@@ -0,0 +1,165 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h> 
+
+
+int count_words(const char *str, const char *charset) 
+{
+    int i = 0;
+    int j = 0;
+    int nbwrd = 0;
+    int inout = 0;    
+    
+
+    while (str[i] != '\0')
+    {   
+        j = 0;
+        int delim = 0;
+
+        while (charset[j] != '\0')
+        {
+            if(str[i] == charset[j])
+                delim = 1;
+
+            j++;
+        }
+
+        if (delim == 1)
+            inout = 0;
+        else if (inout == 0)
+        {
+            nbwrd++;
+            inout = 1;
+        }
+
+        i++;
+    }
+
+    return nbwrd;
+}
+
+char *copy_word (const char *str, int deb, int fin)
+{
+    int k = 0;
+    int taille = fin - deb;
+    char *mot = (char*)malloc((taille + 1 ) * sizeof(char));
+
+    if (mot == NULL)
+        return NULL;
+
+    while (k < taille)
+    {
+        mot[k] = str[deb + k];
+        k++;
+    }
+
+    mot[k] = '\0';
+
+    return mot;
+}
+
+char **ft_split(char *str, char *charset)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int deb = 0;
+    int fin = 0;
+    int nbword = count_words(str, charset);
+        
+    
+    char **chaine = (char**)malloc((nbword + 1 ) * sizeof(char*));
+
+   if (chaine == NULL)
+        return NULL;
+
+    while (str[i] != '\0')
+    {
+        while (str[i] != '\0')
+        {   
+            j = 0;
+            int delim = 0;
+
+            while (charset[j] != '\0')
+            {
+                if(str[i] == charset[j])
+                    delim = 1;
+
+                j++;
+            }
+            if (delim == 0)
+                break;
+
+            i++;
+        }
+        deb = i;
+
+        while (str[i] != '\0')
+        {   
+            j = 0;
+            int delim = 0;
+
+            while (charset[j] != '\0')
+            {
+                if(str[i] == charset[j])
+                    delim = 1;
+
+                j++;
+            }
+            if (delim == 1)
+                break;
+
+            i++;
+        }
+        fin = i;
+
+      
+
+    if (deb < fin)
+    {
+        chaine[k] = copy_word(str, deb, fin);
+        if (chaine[k] == NULL)
+        {
+            int l = 0;
+            while (l < k)
+            {
+                free(chaine[l]);
+                l++;
+            }
+
+            free(chaine);
+
+            return NULL;
+
+        }
+        
+        k++;
+    }
+}
+
+    chaine[k] = NULL; 
+
+    return chaine;
+    
+}
+
+int main() {
+    char *str = "La! voisine, est fraiche";
+    char *charset = " ,-.!?";
+
+    char **result = ft_split(str, charset);
+
+    int i = 0;
+
+    while (result[i] != NULL) 
+    {
+        printf("Le mot est : %s\n", result[i]);
+        free(result[i]);
+        i++;
+    }
+
+    free(result); 
+
+    return 0;
+} 
