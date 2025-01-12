7b69a1e0067c01a700ceab05726976f048418df2
Mymushy <148210530+Mymushy@users.noreply.github.com>
2025-01-12 15:51:39 +0100

    Update C05.c

--- C07.C/C05.c
+++ C07.C/C05.c
@@ -145,7 +145,7 @@ char **ft_split(char *str, char *charset)
 }
 
 int main() {
-    char *str = "La! voisine, est sympa";
+    char *str = "Le! voisin, est sympa";
     char *charset = " ,-.!?";
 
     char **result = ft_split(str, charset);
